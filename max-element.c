3f58ff6325103c6047721fdb47688f8b1cddd4ad
Dgrady3 <joegrady1@gmail.com>
2014-12-22 22:28:20 -0500

    Added comments breaking down problem for cs50 Hw assignment

--- max-element.c
+++ max-element.c
@@ -8,15 +8,21 @@ int main()
   scanf("%d", &size);
 
   printf("Enter %d integers\n", size);
-
+//user inputs size of list
   for (c = 0; c < size; c++)
     scanf("%d", &array[c]);
+//while iterating through array
+//scanf assigns the decimal to each element
 
   maximum = array[0];
+//assign first element to a var of maximum
 
   for (c = 1; c < size; c++)
   {
     if (array[c] > maximum)
+      //if any element with array is greater than maximum
+      //assign it to maximum and assign its location to the variable
+      //by looking at what c is and adding 1 bc we're dealing w arrays
     {
        maximum  = array[c];
        location = c+1;
