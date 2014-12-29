0bff08964924c1e674a438efaa2c3367b606327a
Dgrady3 <joegrady1@gmail.com>
2014-12-28 23:20:11 -0500

    Added comments to problem

--- swap_numbers.c
+++ swap_numbers.c
@@ -2,13 +2,19 @@
 
 int main()
 {
+//user enters 2 variables
    int x, y, temp;
 
    printf("Enter the value of x and y\n");
    scanf("%d%d", &x, &y);
-
+//gives data before switching nums
    printf("Before Swapping\nx = %d\ny = %d\n",x,y);
 
+//3 variables are used as a shuffling process
+//x is a temp
+//y moves to the x location
+//temp is pushed to the y location
+//therefore creating an easy trade of values or "hand-offs"
    temp = x;
    x    = y;
    y    = temp;
@@ -17,3 +23,5 @@ int main()
 
    return 0;
 }
+
+//next up, completing the swap w only two variables
